0edefb273942a5963d44d0f309fd3b8fbd69a22f
AnshuPtl <anshuptl@outlook.com>
2023-03-23 12:17:26 +0530

    merge other

--- Task/file.py
+++ Task/file.py
@@ -2,4 +2,4 @@ from package import module
 
 print(module.mul(10,8))
 
-print(module.expo(3))
+print(module.expo(10,3))
--- Task/package.py
+++ Task/package.py
@@ -3,5 +3,5 @@ class module:
     def mul(a,b):
         return a*b
 
-    def expo(a):
-        return expo**a
+    def expo(a,b):
+        return a**b
