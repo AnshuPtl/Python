b5de850a3479c095e7d0d21b20f36f2fecbe6e0c
AnshuPtl <anshuptl@outlook.com>
2023-04-09 19:26:13 +0530

    merge

--- Django/myapp/urls.py
+++ Django/myapp/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-       
+    path('about',views.about, name='about'),
+    path('contact', views.contact, name="contact"),
+    path('products',views.products, name='products'),
+    path('feedback',views.feedback, name="feedback"),
+    path('Signup',views.signup,name="signup"),
 ]
--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -1,7 +1,38 @@
 from django.shortcuts import render
-
+from .models import User
 # Create your views here.
 
 def index(request):
 	return render(request,'index.html')
 
+def about(request):
+	return render(request,'about.html')
+	
+def contact(request):
+	return render(request,'contact.html')
+
+def products(request):
+	return render(request,'products.html')
+
+def feedback(request):
+	return render(request,'feedback.html')
+
+def signup(request):
+	if request.method=="POST":
+		try:
+			user=User.objects.get(email=request.POST['email'])
+			print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> USING GET METHOD:: ",user)
+			msg="User already exists !!!"
+			return render(request,'Signup.html',{'msg':msg})
+
+		except:
+			User.objects.create(
+				name=request.POST['name'],
+				email=request.POST['email'],
+				pswd=request.POST['pswd'],
+				)
+			msg1="Registration Successfull"
+			return render(request,'Signup.html',{'msg1':msg1})
+
+	else:
+		return render(request,'Signup.html')
