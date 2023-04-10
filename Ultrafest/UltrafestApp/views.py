61b74dc5b8bab947069653b6690f01bfb7b3eed6
AnshuPtl <anshuptl@outlook.com>
2023-04-10 18:13:58 +0530

    merge

--- Ultrafest/UltrafestApp/views.py
+++ Ultrafest/UltrafestApp/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
+from .models import User
+
 
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
+
+def signup(request):
+	if request.method == "POST":
+		try:
+			User.objects.get(Email=request.POST['email'])
+			msg="User Already Registered !!!"
+			return render(request,'signup.html',{'msg':msg})
+		except:
+			User.objects.create(
+				Name = request.POST['Name'],
+				Email = request.POST['email'],
+				PhoneNumber = request.POST['Phone Number'],
+				Password = request.POST['password'],
+				)
+			msg1="Registration Successfull"
+			return render(request,'signup.html',{'msg1':msg1})
+	else:
+		return render(request,'signup.html')
